d5903f248cc5229ca0c126205d68a59268de41c9
Иван <103172483+xaivan95@users.noreply.github.com>
2026-04-27 20:04:29 +0300

    Create pet_functions.php file with pet management CRUD functions

---
+++ includes/pet_functions.php
@@ -0,0 +1,42 @@
+<?php
+
+// pet_functions.php
+
+/**
+ * Create a new pet record
+ * @param array $petData
+ * @return bool
+ */
+function createPet($petData) {
+    // Logic to create pet
+}
+
+/**
+ * Read pet record by ID
+ * @param int $petId
+ * @return array|null
+ */
+function readPet($petId) {
+    // Logic to read pet
+}
+
+/**
+ * Update existing pet record
+ * @param int $petId
+ * @param array $petData
+ * @return bool
+ */
+function updatePet($petId, $petData) {
+    // Logic to update pet
+}
+
+/**
+ * Delete pet record
+ * @param int $petId
+ * @return bool
+ */
+function deletePet($petId) {
+    // Logic to delete pet
+}
+
+?>
