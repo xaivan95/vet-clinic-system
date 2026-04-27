366eb60f373b079ad140be8bfb266aeb73e7da2d
Иван <103172483+xaivan95@users.noreply.github.com>
2026-04-27 20:04:40 +0300

    Create veterinarian_functions.php with veterinarian management functions

---
+++ includes/veterinarian_functions.php
@@ -0,0 +1,28 @@
+<?php
+
+// Function to add a new veterinarian
+function add_veterinarian($name, $specialty, $contact_info) {
+    // Code to add veterinarian to the database
+}
+
+// Function to get a veterinarian by ID
+function get_veterinarian($id) {
+    // Code to retrieve veterinarian details from the database by ID
+}
+
+// Function to update a veterinarian's information
+function update_veterinarian($id, $name, $specialty, $contact_info) {
+    // Code to update veterinarian details in the database
+}
+
+// Function to delete a veterinarian
+function delete_veterinarian($id) {
+    // Code to remove veterinarian from the database
+}
+
+// Function to list all veterinarians
+function list_veterinarians() {
+    // Code to retrieve and return a list of all veterinarians
+}
+
+?>
