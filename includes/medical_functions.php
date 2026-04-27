9820831ab032771047a9055ba442ae541659e181
Иван <103172483+xaivan95@users.noreply.github.com>
2026-04-27 20:04:51 +0300

    Add medical functions for medical records and vaccinations

---
+++ includes/medical_functions.php
@@ -0,0 +1,23 @@
+<?php
+
+// Function to retrieve medical records
+function getMedicalRecords($petId) {
+    // Code to retrieve medical records from the database
+}
+
+// Function to add a medical record
+function addMedicalRecord($petId, $record) {
+    // Code to add a new medical record to the database
+}
+
+// Function to retrieve vaccination records
+function getVaccinationRecords($petId) {
+    // Code to retrieve vaccination records from the database
+}
+
+// Function to add a vaccination record
+function addVaccinationRecord($petId, $vaccination) {
+    // Code to add a new vaccination record to the database
+}
+
+?>
